1659a90c38c07595dcd9e1992fb7bb15f44e278f
吴海铭 <freehuntman@gmail.com>
2017-03-30 22:24:35 +0800

    implement posts Edit/Delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,11 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!, :only => [:new, :create]
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -20,11 +25,31 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: 'Update Success'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to account_posts_path, alert: 'Posts deleted'
+  end
+
+
+
 
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
 end
